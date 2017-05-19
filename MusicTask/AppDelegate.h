35b049c7565fd1b90eabd2fa51c29ba1843d0a51
apple <apple@apples-Mac-mini.local>
2017-05-19 12:13:37 +0530

    Initial Commit

---
+++ MusicTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicTask
+//
+//  Created by apple on 19/05/17.
+//  Copyright © 2017 Mizpahsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicTask
+//
+//  Created by apple on 19/05/17.
+//  Copyright © 2017 Mizpahsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
